566d487f818120cd8830a18c69f831ba2e17859b
Naxotek <ogorekleon@gmail.com>
2021-11-07 23:47:20 +0100

    TaK

--- shootero/Assets/CometFall.cs
+++ shootero/Assets/CometFall.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class CometFall : MonoBehaviour
 {
     public Rigidbody2D rigidbodyComet;
-    float strength = 10;
+    float strength = 50;
     private void Start()
     {
-        Vector3 v3Force = strength * transform.forward;
-        Debug.Log(transform.forward);
-        Debug.Log(v3Force);
-        rigidbodyComet.AddForce(v3Force);
+        rigidbodyComet.AddRelativeForce(new Vector2(0, strength * Time.deltaTime));
     }
 }
