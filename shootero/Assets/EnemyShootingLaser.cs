1ea907e4a931b526b0abc4641313eb2ec5b85f0b
SaphireChimp470 <trolekb07@gmail.com>
2021-11-08 21:54:20 +0100

    Folder managment

--- shootero/Assets/EnemyShootingLaser.cs
+++ shootero/Assets/EnemyShootingLaser.cs
@@ -6,17 +6,24 @@ public class EnemyShootingLaser : MonoBehaviour
 {
     public LineRenderer rend;
     public GameObject firePoint;
+    public LayerMask layerMask;
 
     private void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.up) * 50f);
+        RaycastHit2D hit;
+        Physics2D.Raycast(transform.position, transform.right * 50, Mathf.Infinity, layerMask);
+        if (!(hit = Physics2D.Raycast(transform.position, transform.right * 50, Mathf.Infinity, layerMask)))
+        {
+            hit.point = -transform.up * 20;
+        }
+
         Debug.Log(hit.point);
-        
+
         rend.SetPosition(0, firePoint.transform.position);
         rend.SetPosition(1, hit.point);
         //if (hit.collider.gameObject.CompareTag("Player"))
         //{
-            //hp -= 1;
+        //hp -= 1;
         //}
     }
 }
