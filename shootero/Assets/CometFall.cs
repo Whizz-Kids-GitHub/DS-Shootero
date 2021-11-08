ca9f2fb068fe0ea813eef7ab5622dd323ae1ee62
Naxotek <ogorekleon@gmail.com>
2021-11-08 21:45:35 +0100

    Falling Block

--- shootero/Assets/CometFall.cs
+++ shootero/Assets/CometFall.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CometFall : MonoBehaviour
 {
     public Rigidbody2D rigidbodyComet;
-    float strength = 500;
+    float strength = 900;
     private void Start()
     {
         rigidbodyComet.AddForce(transform.right * strength);
