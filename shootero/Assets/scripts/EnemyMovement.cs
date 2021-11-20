4768e8588e814afa9a4bfd7bd2768150cffe1975
SaphireChimp470 <trolekb07@gmail.com>
2021-11-20 13:22:17 +0100

    racoon update

--- shootero/Assets/scripts/EnemyMovement.cs
+++ shootero/Assets/scripts/EnemyMovement.cs
@@ -13,23 +13,30 @@ public class EnemyMovement : MonoBehaviour
     private float duration = 2;
     public Vector3 targetPosition;
 
+    private bool once;
+
     private void Start()
     {
+        once = true;
         targetSpot = new GameObject("targetSpotEnemy");
         StartCoroutine(Move());
     }
 
     IEnumerator Move()
     {
-        float time = 0;
-        Vector3 startPosition = transform.position;
-
-        while (time < duration)
+        if (once)
         {
-            transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
-            time += Time.deltaTime;
+            float time = 0;
+            Vector3 startPosition = transform.position;
+
+            while (time < duration)
+            {
+                transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+                time += Time.deltaTime;
+            }
+            transform.position = targetPosition;
+            once = false;
         }
-        transform.position = targetPosition;
 
         StartCoroutine(Move2());
         do
--- shootero/Assets/scripts/EnemyShootingLaser.cs
+++ shootero/Assets/scripts/EnemyShootingLaser.cs
@@ -7,10 +7,19 @@ public class EnemyShootingLaser : MonoBehaviour
     public LineRenderer rend;
     public GameObject firePoint;
     public LayerMask layerMask;
+    private Transform player;
 
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
     private void Update()
     {
-        if (Physics2D.Raycast(transform.position, transform.up))
+        var dir = player.position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle + 90, Vector3.forward);
+
+        if (Physics2D.Raycast(transform.position, Vector2.up * 111))
         {
             RaycastHit2D hit = Physics2D.Raycast(firePoint.transform.position, Vector2.up);
             rend.SetPosition(0, firePoint.transform.position);
@@ -19,7 +28,7 @@ public class EnemyShootingLaser : MonoBehaviour
         else
         {
             rend.SetPosition(0, firePoint.transform.position);
-            rend.SetPosition(1, transform.up *  5f);
+            rend.SetPosition(1, Vector2.up *  5f);
         }
 
         //if (hit.collider.gameObject.CompareTag("Player"))
